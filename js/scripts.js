b19f0c7aeec2ecc05600a1adb190e10b9dada295
Adela Darmansyah and Krista Rutz <adela@>
2020-01-22 16:48:02 -0800

    add a prime number calculator that does not yet return a value

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,72 @@ var palindromeCheck = function(string) {
 
 
 
+var primeCheck = function(number) {
+  var list = [];
+  for (var i = 2; i <= number; i++) {
+    list.push(i);
+  }
+  console.log(list);
+
+
+  // //for (var prime = 2; prime <= number; prime = primeList[primeList.length-1]) {
+  //   for (var j = 0; j <= number; j++){
+  //     for (var i = 0; i < list.length; i++) {
+  //       if (list[i] % prime != 0) {
+  //         editedList.push(list[i]);
+  //       }
+  //     }  
+  //   prime = editedlist[j];
+  //   console.log(prime);
+  //   list = editedList;
+  //   console.log(list);
+  //   primeList.push(prime);
+  // }
+  
+  // console.log(primeList);
+  // console.log(editedList);
+
+      var prime = 2;
+      var editedList = [];
+      for (var i = 0; i < list.length; i++) {
+        if (list[i] % prime != 0) {
+          editedList.push(list[i]);
+        }
+      }
+      var primeList = [prime];
+
+      prime = 3;
+      var editedList2 = [];
+      for (var i = 0; i < editedList.length; i++) {
+        if (editedList[i] % prime != 0) {
+          editedList2.push(editedList[i]);
+        }
+      }
+      primeList.push(prime);
+
+      prime = 5;
+      var editedList3 = [];
+      for (var i = 0; i < editedList2.length; i++) {
+        if (editedList2[i] % prime != 0) {
+          editedList3.push(editedList2[i]);
+        }
+      }
+      primeList.push(prime);
+
+      prime = 7;
+      var editedList4 = [];
+      for (var i = 0; i < editedList3.length; i++) {
+        if (editedList3[i] % prime != 0) {
+          editedList4.push(editedList3[i]);
+        }
+      }
+      primeList.push(prime);
+  
+  console.log(primeList);
+  console.log(editedList4);
+
+
+}
 
 
 $(document).ready(function() {
@@ -114,6 +180,16 @@ $(document).ready(function() {
     
     $("#palindromeOutput").text(palindromeCheck(userInput));
   })
+
+
+  //Prime Sifting
+  $("#primeForm").submit(function(event) {
+    event.preventDefault();
+    var primeInput = $("#primeInput").val();
+    
+    $("#primeOutput").text(primeCheck(primeInput));
+
+  });
 
 
 })
